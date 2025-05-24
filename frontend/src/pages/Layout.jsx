5ca9903056c40e1a93b2b892ed25357cba18f5c9
shivam raj <rajshivam691@gmail.com>
2025-05-24 15:41:50 +0530

    add changes

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -5,7 +5,7 @@ const Layout = () => {
   return (
     <div className="flex flex-col md:flex-row">
       <Sidebar />
-      <main className="flex-1 mt-16 md:mt-0 md:ml-[150px] p-4">
+      <main className="flex-1 mt-16 md:mt-0 md:ml-[100px] p-4">
         <Outlet />
       </main>
     </div>
