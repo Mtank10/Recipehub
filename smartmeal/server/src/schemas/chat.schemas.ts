1aa4a9267d3dbbd1f8ecf1a1240576ce291f49cd
shivam raj <rajshivam691@gmail.com>
2025-07-02 12:27:11 +0530

    add micro service smartmeal

--- smartmeal/server/src/schemas/chat.schemas.ts
+++
@@ -1,6 +0,0 @@
-import Joi from 'joi';
-
-export const chatSchema = Joi.object({
-  message: Joi.string().min(1).required(),
-  context: Joi.object().optional()
-});
--- smartmeal/server/src/schemas/nutrition.schemas.ts
+++
@@ -1,9 +0,0 @@
-import Joi from 'joi';
-
-export const nutritionAnalysisSchema = Joi.object({
-  recipe: Joi.object({
-    title: Joi.string().required(),
-    ingredients: Joi.array().required(),
-    instructions: Joi.array().required()
-  }).required()
-});
